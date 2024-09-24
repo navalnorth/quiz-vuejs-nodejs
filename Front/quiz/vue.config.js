b1262cdc6ddf5bb86e620b0019351233e0195154
navalnorth <moha_naval@live.com>
2024-09-24 22:55:09 +0200

    update all

--- Front/quiz/vue.config.js
+++ Front/quiz/vue.config.js
@@ -1,10 +1,15 @@
+const webpack = require('webpack');
+
 module.exports = {
+  // Indique le chemin d'accès pour le déploiement
+  publicPath: process.env.NODE_ENV === 'production' ? '/<nom-du-projet>/' : '/',
+  
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
         __VUE_OPTIONS_API__: JSON.stringify(true),
         __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false), // Ajoutez cette ligne
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false), // Cette ligne est déjà ajoutée
       })
     ]
   }
